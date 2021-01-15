eccec1a58cdb4a821037f2180b60826fb54b0549
Liz Burke <liz.burke.chi@gmail.com>
2021-01-15 21:53:16 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,9 +7,11 @@ row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
-
+    if src[row_index][element_index]
+      p src[row_index][element_index]
+    end
     element_index += 1
-
   end
+  row_index += 1
 end
 end
